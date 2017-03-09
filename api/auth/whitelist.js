eeb398d978fe211d8126b47e37b21229831a0b10
Pritin Tyagaraj <pritin.tyagaraj@sap.com>
2017-03-09 13:29:30 +0530

    Placeholder welcome page with login button

--- api/auth/whitelist.js
+++ api/auth/whitelist.js
@@ -1,11 +1,35 @@
 'use strict';
 
+var allowed = {
+    startsWith: [
+        '/welcome'
+    ],
+    exact: {
+        '/auth/facebook/login': true,
+        '/auth/facebook/redirect': true,
+        '/auth/facebook/handle_token': true,
+        '/': true,
+        '/welcome/index.html': true,
+        '/welcome/bootstrap/script.js': true,
+        '/favicon.ico': true,
+        '/fb-access-denied.html': true
+    }
+};
+
 module.exports = {
-    '/auth/facebook/login': true,
-    '/auth/facebook/redirect': true,
-    '/auth/facebook/handle_token': true,
-    '/': true,
-    '/index.html': true,
-    '/favicon.ico': true,
-    '/fb-access-denied.html': true
+    isAllowed: function(path) {
+        //Check starts with
+        var bAllowed = allowed.startsWith.some(function(each) {
+            if (path.indexOf(each) === 0) {
+                return true;
+            }
+        });
+
+        //Check exact paths
+        if (bAllowed || allowed.exact[path]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
