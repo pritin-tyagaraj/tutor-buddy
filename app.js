1db8ac77b3da4a894403ec1d6945140eb5d62b29
Pritin Tyagaraj <pritin.tyagaraj@sap.com>
2017-05-31 14:42:41 +0530

    Handle 404 response to requests

--- app.js
+++ app.js
@@ -50,6 +50,11 @@ winston.add(winston.transports.Console, {
 server.use(require('./api/middleware/validateRequest').checkUserAuthentication);
 winston.info('Wiring request validation middleware... Done.');
 
+// End requests that we don't know how to handle.
+server.on('ResourceNotFound', function(req, res, err, cb) {
+    res.send(404);
+});
+
 // Setup auth related routes
 require('./api/auth/facebook').initServerRoutes(server);
 winston.info('Setting up facebook auth routes... Done.');
@@ -79,30 +84,16 @@ server.post('/api/v1/batch/:batchId/student/:studentId/payments', require('./api
 server.get('/api/v1/batch/:batchId/payments', require('./api/v1/payment').getPaymentsForBatch);
 server.del('/api/v1/payment/:paymentId', require('./api/v1/payment').deletePayment);
 
-// Google Cloud Health Check paths
-server.get('/_ah/vm_health', (req, res) => {
-    res.json(200);
-});
 
 winston.info('Setting up batch routes... Done.');
 
-// For Letsencrypt verification
-/*
-server.get('/.well-known/acme-challenge/1tvWlI0MKwAojMP-hjlFXmspUY9Nv_MfMKEI-bIfvrE', function(req, res, next) {
-    res.send('1tvWlI0MKwAojMP-hjlFXmspUY9Nv_MfMKEI-bIfvrE.1_e_ljwng33zNsyfnh65muyDOSIk4th_glYrG4RJXnM');
-});
-server.get('/.well-known/acme-challenge/rfYoOSmZYMf1_TDAyBbCvcKyOlTFftW2j6Miy1ZTMFI', function(req, res, next) {
-    res.send('rfYoOSmZYMf1_TDAyBbCvcKyOlTFftW2j6Miy1ZTMFI.1_e_ljwng33zNsyfnh65muyDOSIk4th_glYrG4RJXnM');
-});
-*/
-
 // Serve the static UI resources
-server.get(/^\/?.*/, restify.serveStatic({
+server.get(/\/?.*/, restify.serveStatic({
     directory: './ui',
     default: 'index.html'
 }));
 
 // Start the server
-server.listen(8080, function() {
+server.listen(process.env.PORT || 8080, function() {
     winston.info('%s listening at %s', server.name, server.url);
 });
